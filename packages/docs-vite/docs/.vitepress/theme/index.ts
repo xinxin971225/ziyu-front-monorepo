c8aac9cd94b753fd75f991df15a402cc3c2514b4
xinxu <xin872906473@163.com>
2022-11-30 15:19:55 +0800

    chore: fix build ui docs

--- packages/docs-vite/docs/.vitepress/theme/index.ts
+++ packages/docs-vite/docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import ZiYuUI from 'ziyu-vite-ui'
 import 'ziyu-vite-ui/dist/style.css'
 import { App } from 'vue'
-import 'unocss'
+// import 'unocss'
 // 主题样式
 import 'vitepress-theme-demoblock/theme/styles/index.css'
 // 插件的组件，主要是demo组件
--- packages/docs-vite/vite.config.ts
+++ packages/docs-vite/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import { presetUno, presetIcons, presetAttributify } from 'unocss'
-import UnoCss from 'unocss/vite'
+// import { presetUno, presetIcons, presetAttributify } from 'unocss'
+// import UnoCss from 'unocss/vite'
 // 单纯的vue3.0的包是不支持sfc语法的，这个过程需要编译，也就是plugin的目的
 import vue from '@vitejs/plugin-vue'
 
@@ -8,12 +8,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons() /** 添加图标预设 查找链接 https://icones.js.org/ */,
-      ],
-    }),
+    // UnoCss({
+    //   presets: [
+    //     presetUno(),
+    //     presetAttributify(),
+    //     presetIcons() /** 添加图标预设 查找链接 https://icones.js.org/ */,
+    //   ],
+    // }),
   ],
 })
